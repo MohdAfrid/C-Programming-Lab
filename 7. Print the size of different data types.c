8435aed703ad961ffbe58f6bc37a1157c6676632
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-05-12 23:42:29 +0530

    Create 7. Print the size of different data types.c

---
+++ 7. Print the size of different data types.c
@@ -0,0 +1,39 @@
+int main() 
+
+{
+
+
+
+  int a;
+
+  char b;
+
+  float c;
+
+  double d;
+
+  short e;
+
+  long f;
+
+  long long g;
+
+  printf("This program is written by Mohd Afrid\n");
+
+  printf("size of integer = %d bytes\n", sizeof(a));
+
+  printf("size of char = %d bytes\n", sizeof(b));
+
+  printf("size of float = %d bytes\n", sizeof(c));
+
+  printf("size of double = %d bytes\n", sizeof(d));
+
+  printf("size of short = %d bytes\n", sizeof(e));
+
+  printf("size of long = %d bytes\n", sizeof(f));
+
+  printf("size of long long = %d bytes\n", sizeof(g));
+
+  return 0;
+
+}
