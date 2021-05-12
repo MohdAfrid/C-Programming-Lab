69b1ce9c30178ebf2b5cd99a7ad42f6e2eae7a69
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-05-12 11:04:42 +0530

    Create 2. How to use comment in program.c

---
+++ 2. How to use comment in program.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+// This is a single line comment
+
+/* This is a
+
+   multiline comment
+
+*/
+
+   printf("'//' This is used for single line comment.\n");
+
+   printf("'/*....*/' This is used for multiline comment.");
+
+    return 0;
+
+}
