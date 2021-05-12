3a8997e6d5a6011e964efadac74a8db654f488c9
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-05-12 11:32:02 +0530

    Create How to use a variable in C.c

---
+++ How to use a variable in C.c
@@ -0,0 +1,11 @@
+# <stdio.h>
+
+int main() {
+    int age = 19;
+    char letter = 'A';
+    /*Here age and letter are variables of
+      int and char type respectively*/
+    printf("Letter = %c\n", letter);
+    printf("Age = %d", age);
+    return 0;
+}
