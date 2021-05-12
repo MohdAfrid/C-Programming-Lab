52ccfd7d18d286b9fa14bbd7f7859463be57d96d
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-05-12 22:54:53 +0530

    Create 4. How to print two variable's value in one statement.c

---
+++ 4. How to print two variable's value in one statement.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int a=5;
+
+    int b=6;
+
+    //Here a and b are variables of integer type
+
+    printf("Values of A and B are : %d,%d", a,b);
+
+    
+
+    return 0;
+
+}
