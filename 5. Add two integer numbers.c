f163b5a1c96776db684f33a2315f507fa54d9925
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-05-12 23:03:25 +0530

    Create 5. Add two integer numbers.c

---
+++ 5. Add two integer numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    int a=4;
+
+    int b=2;
+
+    printf("A = 4\n");
+
+    printf("B = 2\n");
+
+    printf("Sum of A and B i.e. A+B = %d",a+b);
+
+    return 0;
+
+}
