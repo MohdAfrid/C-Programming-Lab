dab0d4331c077a704f0cd9d5534fb1ee32291656
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-05-13 20:32:51 +0530

    Create 8. Take input from the user(Square of a number).c

---
+++ 8. Take input from the user(Square of a number).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    
+
+   int number;
+
+   
+
+   printf("This program is written by Mohd Afrid\n");
+
+ 
+
+   printf("Enter a number to get it's square : ");
+
+   
+
+   scanf("%d",&number);
+
+   
+
+   printf("Square of the given number is : %d",number*number);
+
+    
+
+   return 0;
+
+}
