a7d728817f1773ed9681b9a4ade574b20bc1d7f1
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-06-09 13:46:59 +0530

    Create Swap two numbers using temp variable.c

---
+++ Swap two numbers using temp variable.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+    printf("This program is written by Mohd Afrid\n");
+
+    int temp, a, b;
+
+    printf("Enter a: ");
+
+    scanf("%d",&a);
+
+    printf("Enter b: ");
+
+    scanf("%d",&b);
+
+    
+
+    temp=a;
+
+    a=b;
+
+    b=temp;
+
+    
+
+    printf("After swapping,\n");
+
+    printf("Value of a: %d\n",a);
+
+    printf("Value of b: %d",b);
+
+    return 0;
+
+}
