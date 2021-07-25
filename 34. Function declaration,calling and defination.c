2fe014147a07c21cd53b64e2b2e47ca669eed212
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-07-25 22:46:18 +0530

    Create 34. Function declaration,calling and defination.c

---
+++ 34. Function declaration,calling and defination.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void exe(); //function declaration
+int main()
+{
+ printf("This program is written by Mohd Afrid\n");
+ exe(); //function calling
+ printf("We are doing C Programming\n");
+ return 0;
+}
+ 
+void exe() //function definition
+{
+    printf("Hello students...\n");
+ return 0;
+}
