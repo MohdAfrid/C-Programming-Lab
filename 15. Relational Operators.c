df472357bc4e8599de22cec459c34d8d211a0968
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-07-25 12:41:10 +0530

    Create 15. Relational Operators.c

---
+++ 15. Relational Operators.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    printf("This program is written by Mohd Afrid\n");
+    
+    int a=10;
+    int b=20;
+    printf("Value of a = 10\nValue of b = 20\n");
+    //In == operator , if condition is true then it will give 1 otherwise 0
+    a==b;
+    printf("'a==b', Result is %d\n",a==b);
+    //In != operator , if condition is true then it will give 0 otherwise 1
+    a!=b;
+    printf("'a!=b', Result is %d\n",a!=b);
+    //In <= operator , if condition is true then it will give 1 otherwise 0
+    a<=b;
+    printf("'a<=b', Result is %d\n",a<=b);
+    //In >= operator , if condition is true then it will give 1 otherwise 0
+    a>=b;
+    printf("'a>=b', Result is %d\n",a>=b);
+    //In < operator , if condition is true then it will give 1 otherwise 0
+    a<b;
+    printf("'a<b', Result is %d\n",a<b);
+    //In > operator , if condition is true then it will give 1 otherwise 0
+    a>b;
+    printf("'a>b', Result is %d\n",a>b);
+    
+    return 0;
+}
