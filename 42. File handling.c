f7522efa8de9436cf9b1c622a06c56e52fc977f7
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-07-26 00:11:07 +0530

    Create 42. File handling.c

---
+++ 42. File handling.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+   printf("This program is written by Mohd Afrid\n");
+   int num;
+   FILE *fptr1, *fptr2;
+
+   fptr1 = fopen("helloF1.txt","r");
+   fptr2 = fopen("helloF2.txt","w");
+
+   if(fptr1 == NULL || fptr2 == NULL )
+   {
+      printf("Error!");   
+      exit(1);             
+   }
+
+   printf("Lets see the value available in file:");
+   fscanf(fptr1,"%d",&num);
+
+   printf("%d",num);
+   fprintf(fptr2,"%d",num);
+   
+   fclose(fptr1);
+   fclose(fptr2);
+
+   return 0;
+}
