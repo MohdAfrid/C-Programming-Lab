f1769559f178c58cdf59a00d2960fa4ba03bc2b1
MohdAfrid <83690734+MohdAfrid@users.noreply.github.com>
2021-07-25 14:07:42 +0530

    Create 22. Even-Odd.c

---
+++ 22. Even-Odd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+  printf("This program is written by Mohd Afrid\n");
+  
+  int a;
+  printf("Enter a number : ");
+  scanf("%d",&a);
+  if(a%2==0)
+  printf("Given number is Even\n");
+  else    
+  printf("Given number is Odd");
+  return 0;
+} 
